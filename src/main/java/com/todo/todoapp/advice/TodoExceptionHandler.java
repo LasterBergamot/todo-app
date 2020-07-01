8f45b015a886d92e0fc4039ece9daf1d8ee890b8
Andras Torok <>
2020-07-01 13:41:24 +0200

    Added TodoInterceptor.
    Also modified TodoExceptionHandler.

--- src/main/java/com/todo/todoapp/advice/TodoExceptionHandler.java
+++ src/main/java/com/todo/todoapp/advice/TodoExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.todo.todoapp.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +16,22 @@ import javax.validation.ConstraintViolationException;
 @ControllerAdvice
 public class TodoExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = Exception.class)
-    protected ResponseEntity<Object> handleUnexpectedExceptions(RuntimeException runtimeException, WebRequest webRequest) {
-        return handleExceptionInternal(runtimeException, "An unexpected exception occurred!", new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
-    }
+    private static Logger LOGGER = LoggerFactory.getLogger(TodoExceptionHandler.class);
 
-    @ExceptionHandler(value = ConstraintViolationException.class)
-    protected ResponseEntity<Object> handleConstraintViolationExceptions(RuntimeException runtimeException, WebRequest webRequest) {
-        return handleExceptionInternal(runtimeException, "The given input is not valid!", new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+    @ExceptionHandler(value = Exception.class)
+    protected ResponseEntity<Object> handleExceptions(RuntimeException runtimeException, WebRequest webRequest) {
+        StringBuilder stringBuilder = new StringBuilder("Message: An unexpected exception occurred!");
+
+        if (runtimeException instanceof DuplicateKeyException) {
+            stringBuilder = new StringBuilder("Message: A record with this key already exists!");
+        } else if (runtimeException instanceof ConstraintViolationException) {
+            stringBuilder = new StringBuilder("Message: The given input is not valid!");
+        }
+
+        stringBuilder.append("\n").append("Original message: ").append(runtimeException.getMessage());
+
+        LOGGER.error(stringBuilder.toString());
+
+        return handleExceptionInternal(runtimeException, stringBuilder.toString(), new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
     }
 }
