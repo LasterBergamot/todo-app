231aa9ad5c8d3099c2f01e3f330d528767af12a9
Andras Torok <>
2020-07-02 12:52:30 +0200

    Login now works with Google as well.

--- src/main/java/com/todo/todoapp/controller/rest/UserRestController.java
+++ src/main/java/com/todo/todoapp/controller/rest/UserRestController.java
@@ -1,6 +1,7 @@
 package com.todo.todoapp.controller.rest;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,10 @@ public class UserRestController {
 
     @GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("login", principal.getAttribute("login"));
+        String name = (principal.getAuthorities().size() == 4)
+                ? String.valueOf(((DefaultOidcUser) principal).getIdToken().getClaims().get("name"))
+                : principal.getAttribute("login");
+
+        return Collections.singletonMap("name", name);
     }
 }
