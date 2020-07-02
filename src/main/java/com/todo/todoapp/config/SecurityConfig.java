6bfadd90658ac55fae92f3a8e9aae87dac522187
Andras Torok <>
2020-07-02 11:07:50 +0200

    Logout now works.

--- src/main/java/com/todo/todoapp/config/SecurityConfig.java
+++ src/main/java/com/todo/todoapp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,10 +14,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http.authorizeRequests(
-                a -> a.antMatchers("/", "/error", "/webjars/**").permitAll()
+                authorize -> authorize.antMatchers("/", "/error", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
+        ).logout(
+                logout -> logout.logoutSuccessUrl("/").permitAll()
+        ).csrf(
+         csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
         ).exceptionHandling(
-                e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                exception -> exception.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
         ).oauth2Login();
         //formatter:on
     }
