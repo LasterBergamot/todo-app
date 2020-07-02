001e9383a3738727ae255c81c36defce955114fa
Andras Torok <>
2020-07-02 16:36:36 +0200

    Update TodoExceptionHandler.java

--- src/main/java/com/todo/todoapp/advice/TodoExceptionHandler.java
+++ src/main/java/com/todo/todoapp/advice/TodoExceptionHandler.java
@@ -16,11 +16,11 @@ import javax.validation.ConstraintViolationException;
 @ControllerAdvice
 public class TodoExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(TodoExceptionHandler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TodoExceptionHandler.class);
 
     @ExceptionHandler(value = Exception.class)
     protected ResponseEntity<Object> handleExceptions(RuntimeException runtimeException, WebRequest webRequest) {
-        StringBuilder stringBuilder = new StringBuilder("Message: ");
+        StringBuilder stringBuilder = new StringBuilder("Error message: ");
 
         if (runtimeException instanceof DuplicateKeyException) {
             stringBuilder.append("A record with this key already exists!");
@@ -30,10 +30,9 @@ public class TodoExceptionHandler extends ResponseEntityExceptionHandler {
             stringBuilder.append("An unexpected exception occurred!");
         }
 
-        stringBuilder.append("\n").append("Original message: ").append(runtimeException.getMessage());
+        String errorMessage = stringBuilder.toString();
+        LOGGER.error(errorMessage);
 
-        LOGGER.error(stringBuilder.toString());
-
-        return handleExceptionInternal(runtimeException, stringBuilder.toString(), new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+        return handleExceptionInternal(runtimeException, errorMessage, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
     }
 }
